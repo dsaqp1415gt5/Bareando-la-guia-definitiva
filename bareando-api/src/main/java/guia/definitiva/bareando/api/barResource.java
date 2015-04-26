da613ccfe3247d50b74c66b92945bac2db417c73
marc <marc@Mojon.lan>
2015-04-26 23:33:11 +0200

    POST

--- bareando-api/src/main/java/guia/definitiva/bareando/api/barResource.java
+++ bareando-api/src/main/java/guia/definitiva/bareando/api/barResource.java
@@ -7,11 +7,16 @@ import java.sql.Connection;
 import java.sql.PreparedStatement;
 import java.sql.ResultSet;
 import java.sql.SQLException;
+import java.sql.Statement;
 
 import javax.sql.DataSource;
+import javax.ws.rs.Consumes;
 import javax.ws.rs.GET;
+import javax.ws.rs.POST;
 import javax.ws.rs.Path;
 import javax.ws.rs.Produces;
+import javax.ws.rs.ServerErrorException;
+import javax.ws.rs.core.Response;
 
 
 @Path("/bares")
@@ -55,5 +60,52 @@ public class barResource {
 			}
 		}		
 		return bares;
+	}
+	
+	private String INSERT_BAR_QUERY = "insert into bares (id, nombre, descripcion, nota) values (null, ?, ?, ?)";
+	private String GET_BAR_ID_QUERY = "select * from bares where id=?";
+
+	@POST
+	@Consumes(MediaType.BAREANDO_BAR)
+	@Produces(MediaType.BAREANDO_BAR)
+	public bar anadirBar(bar BAR){
+		//validarBar(BAR);
+		
+		Connection conn = null;
+		try {
+			conn = ds.getConnection();
+		} catch (SQLException e) {
+			throw new ServerErrorException("No se ha podido conectar con la base de datos",
+					Response.Status.SERVICE_UNAVAILABLE);
+		}
+		
+		PreparedStatement stmt = null;
+		try{
+			stmt = conn.prepareStatement(INSERT_BAR_QUERY, Statement.RETURN_GENERATED_KEYS);
+			stmt.setString(1, BAR.getNombre());
+			stmt.setString(2, BAR.getDescripcion());
+			stmt.setInt(3, BAR.getNota());
+			stmt.executeUpdate();
+			
+			ResultSet rs = stmt.getGeneratedKeys();
+			if (rs.next()) {
+				int ID = rs.getInt(1); 
+
+				//BAR = getBarById(Integer.toString(ID));
+			} else {
+				// Something has failed...
+			}
+		} catch (SQLException e) {
+			throw new ServerErrorException(e.getMessage(),
+					Response.Status.INTERNAL_SERVER_ERROR);
+		} finally {
+			try {
+				if (stmt != null)
+					stmt.close();
+				conn.close();
+			} catch (SQLException e) {
+			}
+		}
+		return BAR;
 	}
 }
