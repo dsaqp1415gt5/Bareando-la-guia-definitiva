b685948e4d1ef598c2c40392cebafca31548eb1f
marc <marc@Mojon>
2015-05-31 12:54:06 +0200

    readme

--- README.md
+++ README.md
@@ -1,4 +1,17 @@
-https://stackedit.io/editor
+# Comentarios
+- url: *:8080/bareando-api/comentarios
 
----------
----------
+###POST:
+- Content-type --> application/vnd.bareando.api.comentario.collection+json
+- raw: {"id":"0","nick":"makitos666","id_bar":"8","mensaje":"la mona chita mola"}
+- el id es indiferente, no se tiene en cuenta
+- nick debe existir como user regitrado, evidentemente
+- id_bar, tambien debe ser el id de un bar existente, no comentaras un bar que no existe
+mensaje, max 250 caracteres xD
+- retorna el mismo comentario que has añadido, cualquier otra cosa que no sea eso es que algo has hecho mal.
+
+###GET
+- url = url + /id_bar --> id_bar un numero
+- Content-type --> application/vnd.bareando.api.comentario.collection+json
+- id_bar hace referencia a la id de un bar, :|
+- te devuelve una coleccion de comentarios referente al bar del que has puesto la id, tambien paginable, aunque aun no lo hace...
---
+++ www/js/core.js
@@ -0,0 +1,60 @@
+var BASE_URL = "http://147.83.7.200:8080/bareando-api";
+//var BASE_URL = "http://localhost:8080/bareando-api";
+
+function uriObject (){
+    this.url = BASE_URL;
+    this.id = 0;
+    this.nombre = 0;
+    this.minNota = 0;
+    this.maxNota = 0;
+    this.random = 0;
+    this.genero = 0;
+    this.orden = 0;
+    this.perpage = 0;
+    this.page = 0;
+    this.paginas = 0;
+    this.response = 0;
+
+    this.restartUrlParameters = function(){
+        this.url = BASE_URL;
+        this.id = 0;
+        this.nombre = 0;
+        this.minNota = 0;
+        this.maxNota = 0;
+        this.random = 0;
+        this.genero = 0;
+        this.orden = 0;
+        this.perpage = 0;
+        this.page = 0;
+        this.response = 0;
+        this.paginas = 0;
+        this.updateUrl();
+    }
+
+    this.updateUrl = function(){
+        this.url = BASE_URL + '/bares/' + this.id + '-' + this.nombre + '-' + this.minNota + '-' + this.maxNota + '-' + this.random + '-' + this.genero + '-' + this.orden + '-' + this.perpage + '-' + this.page;
+        return this.url;
+    };
+
+    this.makeGetRequest = function(){
+        this.response = makeRequest('GET', this.url);
+        return this.response;
+    };
+}
+
+function makeRequest(type, url){
+    var respuesta;
+    $.ajax({
+        url : url,
+        type : type,
+        async: false,  
+        crossDomain : true,
+        dataType : 'json',
+    }).done(function(data, status, jqxhr) {
+        respuesta = data;
+    }).fail(function(jqXHR, textStatus) {
+        console.log(textStatus);
+        respuesta = textStatus;
+    });
+    return respuesta;
+}
